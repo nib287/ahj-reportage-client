0505c3d4220d416979f3df90322b3a00b41c17c6
Igor Nikolauk <nib287@yandex.ru>
2021-03-21 20:17:54 +0300

    initial commit

---
+++ src/js/api.js
@@ -0,0 +1,43 @@
+export default class API {
+    constructor() {
+        this.container = document.getElementsByClassName('container').item(0);
+    }
+    
+    init() {
+        const eventSource = new EventSource("https://ahj-reportage.herokuapp.com//sse");
+        eventSource.addEventListener('message', (evt) => {
+            const message = JSON.parse(evt.data);
+            this.createMessage(message);
+        });
+
+        eventSource.addEventListener('open', (evt) => {
+            this.createMessage('Игра началась');
+        });
+        eventSource.addEventListener('error', (evt) => {
+            console.log('error');
+        });
+    }
+
+    createMessage(message) {
+        const date = this.getDate();
+        this.container.insertAdjacentHTML('beforeEnd', `
+            <li class="container__item">
+                <span class="container__date">${date}</span>
+                <span class="container__message">${message}</span>
+            </li>`
+        );
+    }
+    
+    getDate() {
+        const options = {
+            year: 'numeric',
+            month: 'numeric',
+            day: 'numeric',
+            timezone: 'UTC',
+            hour: 'numeric',
+            minute: 'numeric',
+        };
+        
+        return  new Date().toLocaleString("ru", options);
+    }
+}
---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+import '../style.css';
+import API from './api.js';
+
+const api = new API()
+api.init();
+
+
