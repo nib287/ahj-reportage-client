9d60a822cfe2c3054a703203a6424e7915ff3782
Igor Nikolauk <nib287@yandex.ru>
2021-03-27 23:16:00 +0300

    fix: add sass

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+import '../css/style.css';
+import ReportageWidget from './ReportageWidget.js';
+
+const reportageWidget = new ReportageWidget('https://ahj-reportage.herokuapp.com/sse');
+reportageWidget.init();
+
+
